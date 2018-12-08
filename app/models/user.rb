3df55a2e7cffeffdd5dc132c3eefb325b37e1c83
william oleson <wnoleson@gmail.com>
2018-12-08 14:25:49 -0700

    signup/login params and pages now available

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
+
+  include LogValidationErrors
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 end
