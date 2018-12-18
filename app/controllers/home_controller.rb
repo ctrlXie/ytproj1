984e50a81fdc4e356b99fdbf402402130d2d167b
william oleson <wnoleson@gmail.com>
2018-12-17 22:00:24 -0700

    setting up vue.JS

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
-    User.all
+    @teams = Team.where("id = ?", current_user.team_id)
+    @projects = Project.where("team_id = ?", current_user.team_id)
+    # shows the teams and projects on dashboard of the current user logged in ("My teams" "My Projects")
   end
 end
