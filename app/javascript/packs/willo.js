c25bb87fdabc84bfcb8acc71881086017c228891
william oleson <wnoleson@gmail.com>
2018-12-23 23:57:33 -0700

    adding event listener to our team form

--- app/javascript/packs/willo.js
+++ app/javascript/packs/willo.js
@@ -7,4 +7,10 @@ document.addEventListener("turbolinks:load", () => {
   Vue.http.header.common['X-CSRF-Token'] = document.querySelector("meta[name='csrf-token']").getAttribute('content')
 
   const form = document.getElementById("team-form")
+
+  if (form != null) {
+    var id = form.dataset.id
+    var team = JSON.parse(form.dataset.team)
+    var user_attributes = JSON.parse(form.dataset.user_attributes)
+  }
 });
