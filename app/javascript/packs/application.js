c041eb288a5ffc0a1af8f5169fb38be0f7594ade
william oleson <wnoleson@gmail.com>
2018-12-23 18:08:40 -0700

    willo.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,4 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-console.log('Hello World from Webpacker')
+console.log('Ah, you think stack overflow is your ally.  you merely adopted the code.  I was born in it.  molded by it')
