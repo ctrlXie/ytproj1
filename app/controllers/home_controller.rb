faca060e6daa49ea22320f7b8df94ced361b56bf
william oleson <wnoleson@gmail.com>
2018-12-26 19:11:32 -0700

    routing path error fixed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @teams = Team.where("id = ?", current_user.team_id)
-    @projects = Project.where("team_id = ?", current_user.team_id)
-    # shows the teams and projects on dashboard of the current user logged in ("My teams" "My Projects")
+    if user_signed_in?
+      @teams = Team.where("id = ?", current_user.team_id)
+      @projects = Project.where("team_id = ?", current_user.team_id)
+      # shows the teams and projects on dashboard of the current user logged in ("My teams" "My Projects")
+    end
+  else
+    root_path
   end
 end
